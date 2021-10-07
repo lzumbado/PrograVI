f0e2196e4d6f75a2b6262bb460d73d0e0c10760a
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-10-06 20:29:53 -0600

    Add project files.

---
+++ Database/dbo/StoreProcedures/TipoIdentificacionObtener.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[TipoIdentificacionObtener]
+      @IdTipoIdentificacion int= NULL
+AS BEGIN
+  SET NOCOUNT ON
+
+  SELECT 
+    E.Descripcion    
+    FROM dbo.TipoIdentificacion E
+    WHERE
+    (@IdTipoIdentificacion IS NULL OR @IdTipoIdentificacion=@IdTipoIdentificacion)
+
+END
---
+++ InitialData/ScriptConfiguration/TipoIdentificacion.sql
@@ -0,0 +1,40 @@
+﻿DROP TABLE IF EXISTS #TipoIdentificacionTemp
+
+SELECT 
+IdTipoIdentificacion, Descripcion INTO #TipoIdentificacionTemp
+FROM (
+VALUES
+(1, 'Cedula'),
+(2, 'Pasaporte'),
+(3, 'Cedula Juridca')
+)AS TEMP (IdTipoIdentificacion, Descripcion)
+
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Descripcion= TM.Descripcion
+FROM Dbo.#TipoIdentificacion P
+INNER JOIN #TipoIdentificacionTemp TM
+    ON P.IdTipoIdentificacion= TM.IdTipoIdentificacion
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.TipoIdentificacion ON
+
+INSERT INTO dbo.#TipoIdentificacion(
+IdTipoIdentificacion, Descripcion)
+SELECT
+IdTipoIdentificacion, Descripcion
+FROM #TipoIdentificacionTemp
+
+
+EXCEPT
+SELECT
+IdTipoIdentificacion, Descripcion
+FROM dbo.#TipoIdentificacion
+
+
+SET IDENTITY_INSERT dbo.TipoIdentificacion OFF
+
+GO
